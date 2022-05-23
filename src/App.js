7100c0bd8abb4a466a963296183b353b1c5673a6
Joaquin <joaquingaldeano7@gmail.com>
2022-05-23 19:08:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+export const App = () => {
+  const [Suma, setSuma] = useState(0);
+
+  useEffect(() => {
+    console.log(`La suma es ${Suma}`)
+    
+    return () => {
+      console.log("Apretaste el boton de sumar")
+    }
+  }, [Suma]);
+  
+  const onClick = ()=>{
+    setSuma(Suma + 1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{Suma}</p>
+      <button onClick={onClick}>+1</button>
     </div>
-  );
+  )
 }
 
-export default App;
